5cf5891eadd50b5b4ddd7fcca8b06784770c1572
sideeq12 <abdwaheed2018@gmail.com>
2021-12-19 17:15:24 +0000

    proto

--- src/components/LandingPage/patners.js
+++ src/components/LandingPage/patners.js
@@ -13,12 +13,12 @@ const Partners = ()=>{
                     </div>
                     <h3>BestBrain Tutorial</h3>
                 </div>
-                <div className="partnerImage">
+                {/* <div className="partnerImage">
                     <div className="image">
                     <img src={tutorial} alt="partners Learners Connect" />
                     </div>
                     <h3>Digi-Tech skill Ltd.</h3>
-                </div>
+                </div> */}
             </div>
         </section>
     )
