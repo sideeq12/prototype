b43718a8105f9e2607ab91285f2ba7759e8362e2
sideeq12 <abdwaheed2018@gmail.com>
2021-12-20 18:26:30 +0000

    new skill

--- src/components/newskill/newskill.js
+++ src/components/newskill/newskill.js
@@ -8,7 +8,30 @@ const NewSkill =()=>{
     return (
         <div className="dashboardWrap">
             <NavLinks /> 
-            <h2>New skill layout</h2>
+            <div className="FileWrap">
+                <div className="fileCover">
+                    <h2>Create a new skill Card</h2>
+                    <form>
+                        <div className="inputCover">
+                            <label for="titile">Title :</label> <br/>
+                            <input type="text" required/>
+                        </div>
+                        <div className="inputCover">
+                            <label for="titile">Details:</label> <br/>
+                            <textarea></textarea>
+                        </div>
+                        <div className="inputCover">
+                            <label for="titile">choose image</label> <br/>
+                            <input type="file"/>
+                        </div>
+                        <div className="inputCover price">
+                            <label for="titile">Price :</label> 
+                            <input type="text" required/>
+                        </div>
+                        <button type="submit">Post template</button>
+                    </form>
+                </div>
+            </div>
             </div>
     )
 }
