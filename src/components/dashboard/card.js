39048881540730a6e40d7bfd7222d3d718f86abd
sideeq12 <abdwaheed2018@gmail.com>
2022-01-02 19:28:41 +0000

    delete

--- src/components/dashboard/card.js
+++ src/components/dashboard/card.js
@@ -1,15 +1,24 @@
 import React from "react";
 import "./card.css"
 
-const Card = ({image, price, text, link}) =>{
+const Card = ({image, price, text, link, del}) =>{
+    const targ = (e)=>{
+        console.log(e.target.parentNode.parentNode.parentNode.parentNode.parentNode)
+    }
     return (
         <div className="card">
+             
             <img src={image} alt="skill image"/>
+           
             <div className="cardDescription">
                 <p>{text} </p>
                 <div className="connect">
+                <div className="deleteIcon" onClick={targ}>
+          {del}
+            </div>
                     <a href={link}>Chat me up</a>
                     <label>{price}</label>
+                    
                 </div>
             </div>
         </div>
--- src/components/dashboard/layout.js
+++ src/components/dashboard/layout.js
@@ -24,9 +24,7 @@ const DashboardLayout = ()=>{
     })
     if(localStorage.getItem("userInfo") !== null){
         const item =localStorage.getItem("userInfo")
-        console.log("the item is", item)
         const email = JSON.parse(item).email
-        console.log("the email is here o", email)
         const User = {
             email : email
         }
@@ -46,7 +44,6 @@ const DashboardLayout = ()=>{
                   axios.post(url2, User, Headers).then(response =>{
                         if(response.data.message === "success"){
                             const List = response.data.data
-                            console.log(List)
                                 setCardList(List)
                         }
                  })  
@@ -63,6 +60,15 @@ const DashboardLayout = ()=>{
 
   
     let skills = userDetails.tags.split(",")
+    function dele(){
+        return (
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="red" class="bi bi-trash" viewBox="0 0 16 16">
+            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+            <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+          </svg>
+        )
+    }
+    let message  = dele()
 
     return (
         <div className="layout">
@@ -96,7 +102,8 @@ const DashboardLayout = ()=>{
                 </div>
             </div>
             <div className="skillList">
-               {cardList.length > 0 &&  cardList.map((data)=><Card image={data.image} key={data._id} text={data.description} price={data.price} link={data.social_link} />)}
+               {cardList.length > 0 &&  cardList.map((data)=><Card image={data.image} key={data._id} del={message}
+                text={data.description} price={data.price} link={data.social_link} />)}
                 {cardList.length === 0 && <h4>You do not any active skill card (0/5 used) </h4>}
             </div>
         </div>
